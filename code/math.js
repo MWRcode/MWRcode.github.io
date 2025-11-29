6c37493b9bc2a7ec9a4d72987f0e9ad611eefee7
MWRcode <michaelreeves2012@outlook.com>
2025-11-29 13:43:06 -1000

    Add tree of life files

---
+++ code/math.js
@@ -0,0 +1,23 @@
+export const math = {
+    randrange: function(a, b) {
+        return Math.random() * (b - a) + a;
+    },
+    lerp: function(x, a, b) {
+        return x * (b - a) + a;
+    },
+    getdist: function(xpos1, ypos1, xpos2, ypos2) {
+        return Math.sqrt((xpos1 - xpos2)**2 + (ypos1 - ypos2)**2);
+    },
+    getsqrdist: function(xpos1, ypos1, xpos2, ypos2) {
+        return (xpos1 - xpos2)**2 + (ypos1 - ypos2)**2;
+    },
+    clamp: function(x, a, b) {
+        return Math.min(Math.max(x, a), b);
+    },
+    sat: function(x) {
+        return Math.min(Math.max(x, 0.0), 1.0);
+    },
+    map: function(x, a, b) {
+        return (x - a) / (b - a);
+    }
+};
---
+++ code/spatialGrid.js
@@ -0,0 +1,91 @@
+export const spatialGrid = class SpatialHash {
+  constructor(dimensions) {
+    this._cells = new Map();
+    this._dimensions = dimensions;
+    this._queryIds = 0;
+  }
+  _getCellIndex(position) {
+    const xIndex = Math.floor(Math.floor(position[0] / this._dimensions[0]) * this._dimensions[0]);
+    const yIndex = Math.floor(Math.floor(position[1] / this._dimensions[1]) * this._dimensions[1])
+
+    return [xIndex, yIndex];
+  }
+  _key(i1, i2) {
+    return i1 + '.' + i2;
+  }
+  newClient(position, dimensions) {
+    const client = {
+      position: position,
+      dimensions: dimensions,
+      indices: null,
+      _queryId: -1,
+    };
+
+    this._insert(client);
+
+    return client;
+  }
+  updateClient(client) {
+    this.remove(client);
+    this._insert(client);
+  }
+  findNear(position, bounds) {
+    const [x, y] = position;
+    const [w, h] = bounds;
+
+    const i1 = this._getCellIndex([x - w / 2, y - h / 2]);
+    const i2 = this._getCellIndex([x + w / 2, y + h / 2]);
+
+    const clients = [];
+    const queryId = this._queryIds++;
+
+    for (let x = i1[0], xn = i2[0]; x <= xn; x += this._dimensions[0]) {
+      for (let y = i1[1], yn = i2[1]; y <= yn; y += this._dimensions[1]) {
+        const k = this._key(x, y);
+
+        if (k in this._cells) {
+          for (let v of this._cells[k]) {
+            if (v._queryId != queryId) {
+              v._queryId = queryId;
+              clients.push(v);
+            }
+          }
+        }
+      }
+    }
+    return clients;
+  }
+  _insert(client) {
+    const [x, y] = client.position;
+    const [w, h] = client.dimensions;
+
+    const i1 = this._getCellIndex([x - w / 2, y - h / 2]);
+    const i2 = this._getCellIndex([x + w / 2, y + h / 2]);
+
+    client.indices = [i1, i2];
+
+    for (let x = i1[0], xn = i2[0]; x <= xn; x += this._dimensions[0]) {
+      for (let y = i1[1], yn = i2[1]; y <= yn; y += this._dimensions[1]) {
+        const k = this._key(x, y);
+        if (!(k in this._cells)) {
+          this._cells[k] = new Set();
+        }
+        this._cells[k].add(client);
+      }
+    }
+  }
+  remove(client) {
+    const [i1, i2] = client.indices;
+
+    for (let x = i1[0], xn = i2[0]; x <= xn; ++x) {
+      for (let y = i1[1], yn = i2[1]; y <= yn; ++y) {
+        const k = this._key(x, y);
+
+        this._cells[k].delete(client);
+      }
+    }
+  }
+  removeAll() {
+    this._cells = new Map();
+  }
+}
